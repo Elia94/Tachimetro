c17bdef0f18513f4f1c87026b85b8fee06851007
Elia94 <moonstone94@gmail.com>
2015-08-12 23:30:33 +0200

    Secondo commit per programma figo arduino

--- Tachimetro.ino
+++ Tachimetro.ino
@@ -9,7 +9,7 @@ float V = 0; //Velocità
 float A = 0; //Accelerazione
 float S = 0; //Distanza percorsa a partire dall'ultima corsa
 float Km = 0; //distanza totale percorsa in Km
-float t1, t2 = 0; //Variabili temporali 1 e 2
+unsigned long t1, t2 = 0; //Variabili temporali 1 e 2
 unsigned long int guard = 0; //Variabile che controlla se sono fermo
 //Clock??
 float timer = 0; //Cronometro
@@ -44,12 +44,14 @@ void Refresh(int sheet){ //funzione che aggiorna i valori visualizzati adesso se
       lcd.setCursor(4,1);
       lcd.print(V);
       }
+    break;
     case 2: {
       lcd.setCursor(4,0);
       lcd.print(Km);
       lcd.setCursor(4,1);
       lcd.print(A);
       }
+      break;
    }
 }
 
@@ -68,24 +70,22 @@ void setup() {
 }
 
 void loop() {
-  /*guard = millis();
+  guard = millis();
   if ((guard - t2) > 2000) { //controllo se sono fermo
     V = 0;
-  }*/
-  
+    Refresh(page);
+      }
   if (digitalRead(reed) == HIGH) { //controllo se il reed ha dato un'impulso e calcolo distanza e velocità
     t1 = t2;
-    t2 = millis()/1000.0;
-    V = ( c / (t2 - t1))*3.6;
+    t2 = millis();
+    V = (c *  3600  / (t2 - t1));
     Km = Km + (c / 1000);
-    lcd.clear();
-    lcd.print(t1);
-    lcd.setCursor(0,1);
-    lcd.print(t2);
-    delay(3000);
-  }
+    Refresh(page);
+    delay(200);
+   }
 
   if (digitalRead(turn_page) == HIGH) {
+    //page = (page + 1) % 2; IN INFORMATICA SI CONTA DA ZERO CAPRA
     if (page == 1) {
       page = 2;
     }
@@ -93,10 +93,21 @@ void loop() {
       page = 1;
     }
     Round(page);
+    delay(100);
 }
 
-Refresh(page);
-delay(15); //andando a 50km/h faccio un giro di ruota ogni 11ms è inutile ripetere più spesso il loop e non rischio di prendere due volte lo stesso passaggio della ruota
 
+//andando a 50km/h faccio un giro di ruota ogni 11ms è inutile ripetere più spesso il loop e non rischio di prendere due volte lo stesso passaggio della ruota
+
+//  lcd.clear();
+//  lcd.print(c);
+//  t1 = millis();
+//  delay(1000);
+//  t2 = millis();
+//  lcd.clear();
+//  lcd.print(t2-t1);
+//  lcd.setCursor(0,1);
+//  lcd.print(c*3600/(t2-t1));
+//  delay(1000);
 }
 
